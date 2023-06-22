29c34aebfb02265d01e6352eedb6609fe58ab621
abakada <martdaynielclanor@gmail.com>
2023-06-22 22:13:30 +0200

    Turn debug off

--- src/app.py
+++ src/app.py
@@ -69,4 +69,4 @@ def api_now():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', '8000')))
+    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', '8000')))
